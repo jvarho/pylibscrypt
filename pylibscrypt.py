ed210e599d7e36188a4bc01807c17ac15305d0a7
Jan Varho <jan@varho.org>
2014-04-27 11:49:32 +0300

    Change p default to 1 and document parameters

--- pylibscrypt.py
+++ pylibscrypt.py
@@ -63,11 +63,25 @@ SCRYPT_MCF_LEN = 125
 
 SCRYPT_N = 1<<14
 SCRYPT_r = 8
-SCRYPT_p = 16
+SCRYPT_p = 1 # Note: Value differs from libscrypt, see below.
 
 
 def scrypt(password, salt, N=SCRYPT_N, r=SCRYPT_r, p=SCRYPT_p):
-    """Derives a 64-byte hash using the scrypt key-derivarion function"""
+    """Derives a 64-byte hash using the scrypt key-derivarion function.
+
+    Memory usage is proportional to N*r. Defaults require about 16 MiB.
+    Time taken is proportional to N*p. Defaults take <100ms of a recent x86.
+
+    The default values are:
+    N -- 2**14 (~16k)
+    r -- 8
+    p -- 1
+
+    The last one differs from libscrypt defaults, but matches the 'interactive'
+    work factor from the original paper. For long term storage where runtime of
+    key derivation is not a problem, you could use 16 as in libscrypt or better
+    yet increase N if memory is plentiful.
+    """
     if not isinstance(password, bytes):
         raise TypeError
     if not isinstance(salt, bytes):
