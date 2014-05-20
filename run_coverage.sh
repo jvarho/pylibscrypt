63db38ef5691ddc98288d5072a31a8eb7877b87a
Jan Varho <jan@varho.org>
2014-05-20 13:51:03 +0300

    Test branch coverage

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 PYTHON=python3
-$PYTHON -m coverage run -m pylibscrypt.tests
-$PYTHON -m coverage run -a -m pylibscrypt.pylibscrypt
-$PYTHON -m coverage run -a -m pylibscrypt.pylibsodium
-$PYTHON -m coverage run -a -m pylibscrypt.pbkdf2
-$PYTHON -m coverage run -a test_fallback.py
+$PYTHON -m coverage run --branch -m pylibscrypt.tests
+$PYTHON -m coverage run --branch -a -m pylibscrypt.pylibscrypt
+$PYTHON -m coverage run --branch -a -m pylibscrypt.pylibsodium
+$PYTHON -m coverage run --branch -a -m pylibscrypt.pbkdf2
+$PYTHON -m coverage run --branch -a test_fallback.py
 PYTHON=python
-$PYTHON -m coverage run -a -m pylibscrypt.tests
-$PYTHON -m coverage run -a -m pylibscrypt.pylibscrypt
-$PYTHON -m coverage run -a -m pylibscrypt.pylibsodium
-$PYTHON -m coverage run -a -m pylibscrypt.pbkdf2
-$PYTHON -m coverage run -a test_fallback.py
-$PYTHON -m coverage report
+$PYTHON -m coverage run --branch -a -m pylibscrypt.tests
+$PYTHON -m coverage run --branch -a -m pylibscrypt.pylibscrypt
+$PYTHON -m coverage run --branch -a -m pylibscrypt.pylibsodium
+$PYTHON -m coverage run --branch -a -m pylibscrypt.pbkdf2
+$PYTHON -m coverage run --branch -a test_fallback.py
+$PYTHON -m coverage report --omit='/usr/*'
 $PYTHON -m coverage annotate
 
