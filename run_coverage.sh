af256c902d0a4e52f05f5a8f72c223bff99ab88a
Jan Varho <jan@varho.org>
2015-03-26 10:09:28 +0200

    Exclude Windows and Darwin from coverage report

--- run_coverage.sh
+++ run_coverage.sh
@@ -11,7 +11,7 @@ $PYTHON -m coverage run --branch -a -m pylibscrypt.pylibscrypt
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pylibsodium
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pbkdf2
 $PYTHON -m coverage run --branch -a test_fallback.py
-$PYTHON -m coverage html --omit='/usr/*'
-$PYTHON -m coverage report --omit='/usr/*'
+$PYTHON -m coverage html --omit='/usr/*,pylibscrypt/libsodium_load.py'
+$PYTHON -m coverage report --omit='/usr/*,pylibscrypt/libsodium_load.py'
 #$PYTHON -m coverage annotate
 
