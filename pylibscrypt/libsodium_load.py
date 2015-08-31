fc13387600b38a140b64195a80d5d21253db35fa
Jan Varho <jan@varho.org>
2015-08-31 13:20:53 +0300

    Retry platform specific methods if ctypes.CDLL fails
    Fixes #15

--- pylibscrypt/libsodium_load.py
+++ pylibscrypt/libsodium_load.py
@@ -17,7 +17,7 @@ import sys
 
 
 def get_libsodium():
-    '''Locate the nacl c libs to use'''
+    '''Locate the libsodium C library'''
 
     __SONAMES = (13, 10, 5, 4)
     # Import libsodium from system
@@ -26,7 +26,11 @@ def get_libsodium():
         sys_sodium = ctypes.util.find_library('libsodium')
 
     if sys_sodium:
-        return ctypes.CDLL(sys_sodium)
+        try:
+            # TODO: verify which exceptions can be returned here
+            return ctypes.CDLL(sys_sodium)
+        except:
+            pass
 
     # Import from local path
     if sys.platform.startswith('win'):
