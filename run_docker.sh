02eb41f168e22b142004933bb3c9aea42b48140d
Jan Varho <jan@varho.org>
2017-08-04 12:53:00 +0300

    Run property-based tests in docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PYTHON=python3.6
 $PYTHON -m coverage run --branch -m pylibscrypt.tests
+$PYTHON -m coverage run --branch -a -m pylibscrypt.test_properties
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pylibscrypt
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pylibsodium
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pbkdf2
@@ -10,6 +11,7 @@ $PYTHON -m coverage run --branch -a test_fallback.py -p
 $PYTHON -m coverage run --branch -a test_fallback.py -e -p
 PYTHON=python
 $PYTHON -m coverage run --branch -a -m pylibscrypt.tests
+$PYTHON -m coverage run --branch -a -m pylibscrypt.test_properties
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pylibscrypt
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pylibsodium
 $PYTHON -m coverage run --branch -a -m pylibscrypt.pbkdf2
