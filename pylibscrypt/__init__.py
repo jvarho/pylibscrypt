80f8e21842cf821af8db8ede907b87911d56c817
Jan Varho <jan@varho.org>
2019-03-09 14:13:38 +0200

    Prepare v1.8.0 release

--- pylibscrypt/__init__.py
+++ pylibscrypt/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2018, Jan Varho
+# Copyright (c) 2014-2019, Jan Varho
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,7 +14,7 @@
 
 """Scrypt for Python"""
 
-__version__ = '1.8.0-git'
+__version__ = '1.8.0'
 
 # First, try hashlib
 _done = False
